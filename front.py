46c1a1554477bdf4649775407ed06c9034044924
islamhani125 <109487950+islamhani125@users.noreply.github.com>
2024-04-18 02:41:22 +0200

    Update front.py
    Fixed the Back function in all the pages 
    added more des functions commented for future work
    fixed the radio button in the FullScan function
    now the selected ip is saved in a variable called ipAddress

--- front.py
+++ front.py
@@ -15,8 +15,11 @@ root.geometry("850x500")
 
 global frame
 global frame1
+
+
 def test_1():
     print("testing")
+
 
 def test_2():
     print("checkpoint")
@@ -26,6 +29,9 @@ def des1():
 
 def des2():
     frame2.destroy()
+
+def des3():
+    frame3.destroy()
 
 def des4():
     frame4.destroy()
@@ -41,32 +47,50 @@ def des7():
 
 def des8():
     frame8.destroy()
+
 def des9():
     frame9.destroy()
 
-#------------------------------------------------------------
-#------------------------------------------------------------
+# def des10():
+#     frame10.destroy()
+#
+# def des11():
+#     frame11.destroy()
+#
+# def des12():
+#     frame12.destroy()
+#
+# def des13():
+#     frame13.destroy()
+
+
+# ------------------------------------------------------------
+# ------------------------------------------------------------
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
+
 def load_users():
-        try:
-            with open("users.json", "r") as file:
-                return json.load(file)
-        except FileNotFoundError:
-            return {}
-        except json.JSONDecodeError:
-            print("Error: Invalid JSON data in users.json file.")
-            return {}
+    try:
+        with open("users.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+    except json.JSONDecodeError:
+        print("Error: Invalid JSON data in users.json file.")
+        return {}
+
 
 def save_users(users):
-        with open("users.json", "w") as file:
-            json.dump(users, file)
-        
+    with open("users.json", "w") as file:
+        json.dump(users, file)
+
+
 def Login():
     global frame
     frame = customtkinter.CTkFrame(master=root)
-    frame.pack(pady=20, padx=60, fill="both", expand=True )
+    frame.pack(pady=20, padx=60, fill="both", expand=True)
+
     def user_login():
         users = load_users()
         username = username_entry.get()
@@ -77,33 +101,36 @@ def Login():
             Homepage()
             return True
         else:
-             CTkMessagebox(title="Error", message="Username or Password Invalid", icon="cancel")
+            CTkMessagebox(title="Error", message="Username or Password Invalid", icon="cancel")
+
     label = customtkinter.CTkLabel(master=frame, text="Login", font=("Roboto", 24))
-    label.pack(pady=(40,10), padx=10)
+    label.pack(pady=(40, 10), padx=10)
 
     username_entry = customtkinter.CTkEntry(master=frame, placeholder_text="Username", width=250)
-    username_entry.pack(pady=(10,10), padx=10)
+    username_entry.pack(pady=(10, 10), padx=10)
 
-    password_entry = customtkinter.CTkEntry(master=frame,show="*", placeholder_text="Password", width=250)
-    password_entry.pack(pady=(10,10), padx=10)
+    password_entry = customtkinter.CTkEntry(master=frame, show="*", placeholder_text="Password", width=250)
+    password_entry.pack(pady=(10, 10), padx=10)
 
     login_button = customtkinter.CTkButton(master=frame, text="Login", command=user_login)
     login_button.pack(pady=12, padx=10)
 
-    button2 = customtkinter.CTkButton(master=frame, text="Sign up",  command=Sign_up)
+    button2 = customtkinter.CTkButton(master=frame, text="Sign up", command=Sign_up)
     button2.pack(pady=12, padx=10)
 
-    button3 = customtkinter.CTkButton(master=frame, text="Forget Password !",  command=Reset, fg_color="transparent")
+    button3 = customtkinter.CTkButton(master=frame, text="Forget Password !", command=Reset, fg_color="transparent")
     button3.pack(pady=2, padx=10)
 
     switch = customtkinter.CTkSwitch(master=frame, text="Remember me?", onvalue="on", offvalue="off")
     switch.pack(pady=12, padx=10)
+
 
 def Sign_up():
     frame.destroy()
     global frame1
     frame1 = customtkinter.CTkFrame(master=root)
     frame1.pack(pady=20, padx=60, fill="both", expand=True)
+
     def create_user():
         users = load_users()
         username = create_user_entry.get()
@@ -118,25 +145,26 @@ def Sign_up():
         print("User created successfully.")
 
     label = customtkinter.CTkLabel(master=frame1, text="Sign up", font=("Roboto", 24))
-    label.pack(pady=(10,10), padx=10)
+    label.pack(pady=(10, 10), padx=10)
 
     create_user_entry = customtkinter.CTkEntry(master=frame1, placeholder_text="Username", width=250)
-    create_user_entry.pack(pady=(10,10), padx=10)
+    create_user_entry.pack(pady=(10, 10), padx=10)
 
-    create_password_entry = customtkinter.CTkEntry(master=frame1,show="*", placeholder_text="Password", width=250)
+    create_password_entry = customtkinter.CTkEntry(master=frame1, show="*", placeholder_text="Password", width=250)
     create_password_entry.pack(pady=(10, 10), padx=10)
 
-
     check_var = customtkinter.StringVar(value="off")
-    checkbox = customtkinter.CTkCheckBox(frame1, text=" I Have Read and Agree to Terms and Conditions", variable=check_var, onvalue="on", offvalue="off")
+    checkbox = customtkinter.CTkCheckBox(frame1, text=" I Have Read and Agree to Terms and Conditions",
+                                         variable=check_var, onvalue="on", offvalue="off")
     checkbox.pack(pady=5, padx=10)
 
     button1 = customtkinter.CTkButton(master=frame1, text="Create Account", command=create_user)
     button1.pack(pady=5, padx=10)
 
-
-    button3 = customtkinter.CTkButton(master=frame1, text="Already Have an Account !", command=lambda:[des1(), Login()], fg_color="transparent")
+    button3 = customtkinter.CTkButton(master=frame1, text="Already Have an Account !",
+                                      command=lambda: [des1(), Login()], fg_color="transparent")
     button3.pack(pady=2, padx=10)
+
 
 def Reset():
     frame.destroy()
@@ -145,22 +173,24 @@ def Reset():
     frame2.pack(pady=20, padx=60, fill="both", expand=True)
 
     label = customtkinter.CTkLabel(master=frame2, text="Password Reset", font=("Roboto", 24))
-    label.pack(pady=(40,10), padx=10)
+    label.pack(pady=(40, 10), padx=10)
 
     entry = customtkinter.CTkEntry(master=frame2, placeholder_text="Username", width=250)
-    entry.pack(pady=(10,10), padx=10)
+    entry.pack(pady=(10, 10), padx=10)
 
     entry2 = customtkinter.CTkEntry(master=frame2, placeholder_text="Email", width=250)
-    entry2.pack(pady=(10,10), padx=10)
+    entry2.pack(pady=(10, 10), padx=10)
 
     entry2 = customtkinter.CTkEntry(master=frame2, placeholder_text="Confirmation Code (OTP)", width=250)
-    entry2.pack(pady=(10,10), padx=10)
+    entry2.pack(pady=(10, 10), padx=10)
 
     button1 = customtkinter.CTkButton(master=frame2, text="Reset", command=test_1)
     button1.pack(pady=12, padx=10)
 
-    button3 = customtkinter.CTkButton(master=frame2, text="Back", command=lambda: [des2(), Login()], fg_color="transparent")
+    button3 = customtkinter.CTkButton(master=frame2, text="Back", command=lambda: [des2(), Login()],
+                                      fg_color="transparent")
     button3.pack(pady=2, padx=10)
+
 
 # ********************************************************************************************
 # ********************************************************************************************
@@ -182,6 +212,12 @@ def Homepage():
 
     button2 = customtkinter.CTkButton(master=frame3, text="History", command=test_1)
     button2.pack(pady=12, padx=10)
+
+    button3 = customtkinter.CTkButton(master=frame3, text="Back", command=lambda: [des3(), Login()],
+                                      fg_color="transparent")
+    button3.pack(pady=2, padx=10)
+
+
 # ********************************************************************************************
 # ********************************************************************************************
 # ********************************************************************************************
@@ -195,11 +231,12 @@ def interface_page():
     frame4 = customtkinter.CTkFrame(master=root)
     frame4.pack(pady=20, padx=60, fill="both", expand=True)
     selected_interface = None
+
     def get_network_interfaces():
         # Get a list of network interfaces
         interfaces = psutil.net_if_addrs()
         return list(interfaces.keys())
-    
+
     def select_interface():
         global selected_interface
         selected_interface = interface_combobox.get()
@@ -223,6 +260,11 @@ def interface_page():
     # Create a button to select the interface
     select_button = customtkinter.CTkButton(master=frame4, text="Select Interface", command=select_interface)
     select_button.pack(pady=10)
+
+    button = customtkinter.CTkButton(master=frame4, text="Back", command=lambda: [des4(), Homepage()],
+                                      fg_color="transparent")
+    button.pack(pady=10, padx=10)
+
 # ********************************************************************************************
 # ********************************************************************************************
 # ********************************************************************************************
@@ -230,42 +272,48 @@ def interface_page():
 # ********************************************************************************************
 # ********************************************************************************************
 def networks_list():
-        nertworks = scan()
-        return nertworks
+    nertworks = scan()
+    return nertworks
+
+
 def scan():
-        try:
-            output = subprocess.check_output(f"iwlist {selected_interface} scan", shell=True).decode()
-        except subprocess.CalledProcessError as e:
-            print("Error:", e)
-            return
-        networks = parse_scan_output(output)
-        return networks
+    try:
+        output = subprocess.check_output(f"iwlist {selected_interface} scan", shell=True).decode()
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+        return
+    networks = parse_scan_output(output)
+    return networks
+
+
 def parse_scan_output(output):
-        networks = []
-        current_network = {}
-        
-        for line in output.split("\n"):
-            if "Cell" in line:
-                if current_network:
-                    networks.append(current_network)
-                current_network = {"ESSID": None, "Channel": None, "Frequency": None, "Quality": None}
-            elif "ESSID:" in line:
-                current_network["ESSID"] = line.split(":")[1].strip().strip('"')
-            elif "Channel:" in line:
-                current_network["Channel"] = line.split(":")[1]
-            elif "Frequency:" in line:
-                frequency_match = re.search(r"(\d+\.\d+) GHz", line)
-                if frequency_match:
-                    current_network["Frequency"] = frequency_match.group(1)
-            elif "Quality=" in line:
-                match = re.search(r"(\d+/\d+)", line)
-                if match:
-                    current_network["Quality"] = match.group(1)
-        
-        if current_network:
-            networks.append(current_network)
-        
-        return [network["ESSID"] for network in networks if network["ESSID"]]
+    networks = []
+    current_network = {}
+
+    for line in output.split("\n"):
+        if "Cell" in line:
+            if current_network:
+                networks.append(current_network)
+            current_network = {"ESSID": None, "Channel": None, "Frequency": None, "Quality": None}
+        elif "ESSID:" in line:
+            current_network["ESSID"] = line.split(":")[1].strip().strip('"')
+        elif "Channel:" in line:
+            current_network["Channel"] = line.split(":")[1]
+        elif "Frequency:" in line:
+            frequency_match = re.search(r"(\d+\.\d+) GHz", line)
+            if frequency_match:
+                current_network["Frequency"] = frequency_match.group(1)
+        elif "Quality=" in line:
+            match = re.search(r"(\d+/\d+)", line)
+            if match:
+                current_network["Quality"] = match.group(1)
+
+    if current_network:
+        networks.append(current_network)
+
+    return [network["ESSID"] for network in networks if network["ESSID"]]
+
+
 def connect_to_wifi(wifi_name, password):
     command = f'nmcli device wifi connect "{wifi_name}" password "{password}"'
     try:
@@ -274,14 +322,21 @@ def connect_to_wifi(wifi_name, password):
     except subprocess.CalledProcessError:
         print(f"Failed to connect to {wifi_name}.")
 
+# ********************************************************************************************
+# ********************************************************************************************
+# ********************************************************************************************
+# ********************************************************************************************
+# ********************************************************************************************
+# ********************************************************************************************
+
 def WifiScan():
     frame4.destroy()
     global frame5
     frame5 = customtkinter.CTkFrame(master=root)
     frame5.pack(pady=20, padx=60, fill="both", expand=True)
-    
 
     selected_network = None
+
     def connect():
         selected_wifi_name = network_combobox.get()  # Get the selected Wi-Fi name
         if selected_wifi_name:  # Check if a Wi-Fi name is selected
@@ -291,7 +346,7 @@ def WifiScan():
 
     scan_button = customtkinter.CTkButton(master=frame5, text="Scan", command=networks_list)
     scan_button.pack(pady=10)
-    network_combobox = customtkinter.CTkComboBox(master=frame5,values=networks_list(), state="readonly")
+    network_combobox = customtkinter.CTkComboBox(master=frame5, values=networks_list(), state="readonly")
     network_combobox.pack(pady=5)
     password_label = customtkinter.CTkLabel(master=frame5, text="Password:")
     password_label.pack(pady=5)
@@ -300,16 +355,17 @@ def WifiScan():
     connect_button = customtkinter.CTkButton(master=frame5, text="Connect", command=connect)
     connect_button.pack(pady=5)
 
-
     button4 = customtkinter.CTkButton(master=frame5, text="Back", command=lambda: [des5(), interface_page()])
     button4.place(relx=0.15, rely=0.93, anchor=tkinter.CENTER)
+
+
 # ********************************************************************************************
 # ********************************************************************************************
 # ********************************************************************************************
 # ********************************************************************************************
 # ********************************************************************************************
 # ********************************************************************************************
-    
+
 # ********************************************************************************************
 # ********************************************************************************************
 # ********************************************************************************************
@@ -331,8 +387,10 @@ def Scan_Page():
     button2 = customtkinter.CTkButton(master=frame6, text="Custom Scan", command=test_1)
     button2.pack(pady=12, padx=10)
 
-    button3 = customtkinter.CTkButton(master=frame6, text="Back", command=lambda: [des5(), WifiScan()], fg_color="transparent")
+    button3 = customtkinter.CTkButton(master=frame6, text="Back", command=lambda: [des6(), WifiScan()],
+                                      fg_color="transparent")
     button3.pack(pady=12, padx=10)
+
 
 def FullScan():
     frame6.destroy()
@@ -343,21 +401,25 @@ def FullScan():
     label = customtkinter.CTkLabel(master=frame7, text="Scanning...", font=("Roboto", 36), text_color="#329983")
     label.pack(pady=20, padx=20)
 
-    radio = customtkinter.CTkRadioButton(frame7, text="Drone ... 141.68.44.205")
+    radio_value = tkinter.StringVar()  #variable to store the selected IP address
+
+    radio = customtkinter.CTkRadioButton(frame7, text="141.68.44.205", variable=radio_value, value="141.68.44.205")
     radio.pack(pady=(6, 3), padx=50, anchor="w")
-    radio2 = customtkinter.CTkRadioButton(frame7, text="Drone ... 141.68.44.205")
+    radio2 = customtkinter.CTkRadioButton(frame7, text="192.168.1.1", variable=radio_value, value="192.168.1.1")
     radio2.pack(pady=(6, 3), padx=50, anchor="w")
-    radio3 = customtkinter.CTkRadioButton(frame7, text="Drone ... 141.68.44.205")
-    radio3.pack(pady=(6, 3), padx=50, anchor="w")
-    radio4 = customtkinter.CTkRadioButton(frame7, text="Drone ... 141.68.44.205")
-    radio4.pack(pady=(6, 3), padx=50, anchor="w")
 
+    def save_ip_address():
+        global ipAddress
+        ipAddress = radio_value.get()
+        print("Selected IP address:", ipAddress)
 
-    button3 = customtkinter.CTkButton(master=frame7, text="Select Drone", command=Test)
+    button3 = customtkinter.CTkButton(master=frame7, text="Select Drone", command=save_ip_address)
     button3.pack(pady=(0, 18), padx=(0, 40), anchor="se", expand=True)
 
     button4 = customtkinter.CTkButton(master=frame7, text="Back", command=lambda: [des7(), Scan_Page()])
     button4.place(relx=0.15, rely=0.93, anchor=tkinter.CENTER)
+
+
 
 def Test():
     frame7.destroy()
@@ -374,8 +436,10 @@ def Test():
     button2 = customtkinter.CTkButton(master=frame8, text="Custom Test", command=CustomTest)
     button2.pack(pady=12, padx=10)
 
-    button3 = customtkinter.CTkButton(master=frame8, text="Back", command=lambda: [des7(), FullScan()],fg_color="transparent")
+    button3 = customtkinter.CTkButton(master=frame8, text="Back", command=lambda: [des8(), FullScan()],
+                                      fg_color="transparent")
     button3.pack(pady=12, padx=10)
+
 
 def CustomTest():
     frame8.destroy()
@@ -398,8 +462,9 @@ def CustomTest():
     button3 = customtkinter.CTkButton(master=frame9, text="Select Drone", command=test_1)
     button3.pack(pady=(0, 18), padx=(0, 40), anchor="se", expand=True)
 
-    button4 = customtkinter.CTkButton(master=frame9, text="Back", command=lambda: [des8(), Test()])
+    button4 = customtkinter.CTkButton(master=frame9, text="Back", command=lambda: [des9(), Test()])
     button4.place(relx=0.15, rely=0.93, anchor=tkinter.CENTER)
+
 
 Login()
 root.mainloop()
